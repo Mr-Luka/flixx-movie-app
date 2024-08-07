ea99fc9868bf2200f8997c013cd5e4443d2a5b6b
Luka <luka.luka.luka93@gmail.com>
2024-08-07 15:00:31 -0700

    highlightActive link function

--- js/script.js
+++ js/script.js
@@ -5,7 +5,18 @@ console.log(global.currentPage) // getting a current page
 // If I click shows, it will show: /shows.html
 
 
-// Init app
+// Highlight active link (movies, TV Shows)
+function highlightActiveLink() {
+    const links = document.querySelectorAll('.nav-link');
+    links.forEach(link => {
+        if (link.getAttribute('href') === global.currentPage) { // checking if the link is on the page we are at
+            link.classList.add('active'); 
+        }
+    })
+}
+
+
+// Init app - checking on what page we are (home, movies, tv-shows, movie details, tv-details)
 function init() {
     switch (global.currentPage) {
         case '/':
@@ -25,6 +36,7 @@ function init() {
             console.log('Search');
             break;
     }
+    highlightActiveLink();
 }
 
 document.addEventListener('DOMContentLoaded', init);
