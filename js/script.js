a7d8918d03701351cb82f638471a913b26cfa25b
Luka <luka.luka.luka93@gmail.com>
2024-08-08 12:40:42 -0700

    displaying the search results

--- js/script.js
+++ js/script.js
@@ -266,7 +266,34 @@ async function search() {
 
 // Function to display search results
 function displaySearchResults(results) {
-    
+        results.forEach(result=> {
+        const div = document.createElement('div');
+        div.classList.add('card'); // global.search.type will be grabbing either movie or show, depending what we are searching
+        div.innerHTML = ` 
+          <a href="${global.search.type}-details.html?id=${result.id}"> 
+            ${
+                result.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
+              class="card-img-top"
+              alt="${global.search.type === 'movie' ? result.title : result.name}"
+            />` : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${global.search.type === 'movie' ? result.title : result.name}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air Date: ${global.search.type 
+                === 'movie' ? result.release_date : result.first_air_date}</small>
+            </p>
+          </div>
+        `;
+        document.querySelector('#search-results').appendChild(div);
+    })
 }
 
 
